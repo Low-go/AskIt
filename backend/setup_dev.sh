ae5d6e6d68161f831fd92486434f39cd99ae4b47
Low-go <lorranalvesgaldino98@gmail.com>
2025-12-16 09:10:18 -1000

    alter and finish setting up docker enviroment for the bakcend

---
+++ backend/setup_dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on error,
+set -e
+
+# Prints ecah command before executing it
+set -x
+
+echo "this is a test run to see if everything is ok!"
+echo "Setting up Django development environment..."
+
+echo "Deleting old migration stuff, strating fresh..."
+rm -rf core/migrations/*
+touch core/migrations/__init__.py
+
+# Apply migrations
+echo "Applying migrations..."
+python manage.py migrate
+
+
+echo "Starting Django server..."
+echo "dev environment up and running"
+python manage.py runserver 0.0.0.0:8000
